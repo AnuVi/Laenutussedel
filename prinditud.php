dd56918b949031bbbb356ba47cc6af03f76ded60
AnuVi <anuviks@gmail.com>
2011-10-17 10:48:31 +0300

    Update prinditud.php

--- prinditud.php
+++ prinditud.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="est" dir="ltr">
+<html lang="et" dir="ltr">
 <head>
 <meta charset="utf-8">
 <meta name="keywords" content="Tellimine sedelkataloogist">
@@ -11,7 +11,7 @@
 
 <body>
 <p>Viimased tellimused sedelkataloogist prinditud</p>
-<p><a href="v_tellimused.php" title="Viimase 30 päeva tellimuste koondtabel"> Vaata viimase kuu aja tellimusi</a></p>
+<p><a href="v_tellimused.php" title="Viimase 30 pï¿½eva tellimuste koondtabel"> Vaata viimase kuu aja tellimusi</a></p>
 <p><a href="https://dev3.utlib.ee/imola/tootaja.php" title="Uued tellimused"> Vaata, kas on uusi tellimusi</a></p>
 </body>
 </html>
